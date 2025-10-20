751de2d943356dc65d8728c520b3b977e50addcb
mick811 <mikkelelvers@outlook.com>
2025-10-20 21:35:19 +0200

    i think it's the last time i change between tailwind and scss

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -8,7 +8,7 @@ import {
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import type { ReactNode } from "react";
 import { authQueryOptions } from "@/lib/queries";
-import styles from "@/index.css?url";
+import styles from "@/styles/base.scss?url";
 
 export const Route = createRootRouteWithContext<{
 	queryClient: QueryClient;
