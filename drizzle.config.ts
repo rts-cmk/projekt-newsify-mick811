b3d0ddc013cf9297a0b344f0dfad75bb78c6a17e
mick811 <mikkelelvers@outlook.com>
2025-10-20 10:43:04 +0200

    feat: setup database

---
+++ drizzle.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from "drizzle-kit";
+import { env } from "@/env";
+
+export default {
+	out: "./drizzle",
+	schema: "./src/db/schema.ts",
+	breakpoints: true,
+	verbose: true,
+	strict: true,
+	dialect: "postgresql",
+	casing: "snake_case",
+	dbCredentials: {
+		url: env.DATABASE_URL,
+	},
+} satisfies Config;
---
+++ src/db/index.ts
@@ -0,0 +1,14 @@
+import { createServerOnlyFn } from "@tanstack/react-start";
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+import { env } from "@/env";
+
+import * as schema from "./schema";
+
+const driver = postgres(env.DATABASE_URL);
+
+const getDatabase = createServerOnlyFn(() =>
+	drizzle({ client: driver, schema, casing: "snake_case" }),
+);
+
+export const db = getDatabase();
