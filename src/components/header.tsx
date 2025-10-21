16496fe5a9dca4e9d21450f5d9da34995e988563
mick811 <mikkelelvers@outlook.com>
2025-10-21 10:40:10 +0200

    feat: add header

---
+++ src/components/header.tsx
@@ -0,0 +1,20 @@
+export const Header = ({ 
+    showSearch = false
+}: {
+    showSearch?: boolean;
+}) => {
+	return (
+		<header className="header">
+			<figure className="header__logo">
+				<img src="/newsify_logo_1.png" alt="Newsify" />
+				<figcaption className="header__logo-text">Newsify</figcaption>
+			</figure>
+
+			{showSearch && (
+				<nav className="header__search">
+					<input type="text" placeholder="Search news" />
+				</nav>
+			)}
+		</header>
+	);
+};
