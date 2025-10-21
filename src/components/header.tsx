e304be270c9afd784a42fda9c06fae568306ba90
mick811 <mikkelelvers@outlook.com>
2025-10-21 13:08:13 +0200

    chore: made logo to a svg

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ export const Header = ({ showSearch = false }: { showSearch?: boolean }) => {
 	return (
 		<header className="header">
 			<figure className="header__logo">
-				<img src="/newsify_logo_1.png" alt="Newsify" />
+				<img src="/newsify_logo_1.svg" alt="Newsify" />
 				<figcaption className="header__logo-text">Newsify</figcaption>
 			</figure>
 
