c24e61bc544eb0bac9e3f7a30985fb917a8319b0
mick811 <mikkelelvers@outlook.com>
2025-10-26 16:55:35 +0100

    add spinner

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,31 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
 import App from "./App";
+import { Spinner } from "./components/spinner";
 
 const router = createBrowserRouter([
     {
-        path: "/",
-        element: <App />,
+        // this element is shown while the route is loading data
+        hydrateFallbackElement: (
+            <div className="fallback">
+                <Spinner />
+            </div>
+        ),
+        children: [
+            {
+                path: "/",
+                element: <App />,
+                loader: async () =>
+                    new Promise((resolve) => { setTimeout(() => {
+                        resolve(null)
+                    }, 1000) }),
+            }
+        ]
     },
 ]);
 
 const root = document.getElementById("root") as HTMLElement;
 
 ReactDOM.createRoot(root).render(
-    <RouterProvider r   outer={router} />
+    <RouterProvider router={router} />
 );
