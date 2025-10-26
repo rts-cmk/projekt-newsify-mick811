daa0fe55c75ea06e0d29cfc70ef01bb8a19ec4b1
mick811 <mikkelelvers@outlook.com>
2025-10-26 16:40:23 +0100

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import App from "./App";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+    },
+]);
+
+const root = document.getElementById("root") as HTMLElement;
+
+ReactDOM.createRoot(root).render(
+    <RouterProvider r   outer={router} />
+);
