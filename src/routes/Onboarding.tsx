3bca311a382f2c2f3bd3018e61cd3c7d6219e229
mick811 <mikkelelvers@outlook.com>
2025-10-27 11:16:09 +0100

    inline onboarding data

--- src/routes/Onboarding.tsx
+++ src/routes/Onboarding.tsx
@@ -1,6 +1,23 @@
 import { useNavigate, useParams } from "react-router";
 import { useSettings } from "../context/settingsContext";
-import { onboarding } from "../lib/data";
+
+const onboarding = [
+	{
+		title: "Stay Connected, Everywhere, Anytime",
+		description:
+			"Welcome to Newsify, your ultimate destination for breaking news, exclusive stories, and tailored content.",
+	},
+	{
+		title: "Become a Savvy Global Citizen.",
+		description:
+			"Discover tailored news that aligns with your interests and preferences. Your personalized news journey awaits!",
+	},
+	{
+		title: "Enhance your News Journey Now!",
+		description:
+			"Be part of our dynamic community and contribute your insights and participate in enriching conversations.",
+	},
+];
 
 export default function Onboarding() {
 	const { settings, updateSettings } = useSettings();
